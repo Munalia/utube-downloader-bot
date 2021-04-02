272a52375b3cba3e38961b0b90f300c09ab958cb
Munalia <69430348+Munalia@users.noreply.github.com>
2021-04-02 23:33:16 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -3,6 +3,9 @@ from pyrogram import Client, Filters, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import user_time
 from config import youtube_next_fetch
 from helper.ytdlfunc import extractYt, create_buttons
+import wget
+import os
+from PIL import Image
 
 ytregex = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
 
@@ -28,21 +31,27 @@ async def ytdl(_, message):
                                      timedelta(minutes=youtube_next_fetch)
 
     except Exception:
-        await message.reply_text("`Failed To Fetch Youtube Data ... Possible Youtube Blocked server IP \n#error`")
+        await message.reply_text("`Failed To Fetch Youtube Data... 😔 \nPossible Youtube Blocked server ip \n#error`")
         return
     buttons = InlineKeyboardMarkup(list(create_buttons(formats)))
-    sentm = await message.reply_text("Processing YouTube URL ..")
+    sentm = await message.reply_text("Processing Youtube Url 🔎 🔎 🔎")
     try:
         # Todo add webp image support in thumbnail by default not supported by pyrogram
         # https://www.youtube.com/watch?v=lTTajzrSkCw
-        await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
+        img = wget.download(thumbnail_url)
+        im = Image.open(img).convert("RGB")
+        output_directory = os.path.join(os.getcwd(), "downloads", str(message.chat.id))
+        if not os.path.isdir(output_directory):
+            os.makedirs(output_directory)
+        thumb_image_path = f"{output_directory}.jpg"
+        im.save(thumb_image_path,"jpeg")
+        await message.reply_photo(thumb_image_path, caption=title, reply_markup=buttons)
         await sentm.delete()
     except Exception as e:
+        print(e)
         try:
             thumbnail_url = "https://telegra.ph/file/8f6c5f15f42c0235bf58f.jpg"
             await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
-            #await message.reply_photo(caption=title, reply_markup=buttons)
         except Exception as e:
             await sentm.edit(
             f"<code>{e}</code> #Error")
-
